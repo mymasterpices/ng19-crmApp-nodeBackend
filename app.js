68c4f77caa82292971347981543b2148eb9c4c7b
mymasterpices <sooraj.sharma@rkjewellers.in>
2025-06-17 18:03:25 +0530

    first commit

--- app.js
+++ app.js
@@ -17,13 +17,14 @@ const port = process.env.PORT || 3000;
 // 3. Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public/browser'))); // Serve Angular frontend
 app.use('/uploads', express.static('uploads'));
 
 
-// 4. Database connection 
+// 4. Database connection
 require('./connection');
 
-// 5. Routes 
+// 5. Routes
 const AuthRoutes = require('./routes/auth');
 const CustomerRoutes = require('./routes/customerRoutes');
 const ChatRoutes = require('./routes/chatRoutes');
